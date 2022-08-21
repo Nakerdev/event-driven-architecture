8952aff3f515b8d7c5d12309816927e14f2f60b3
Antonio Sánchez <antoniojesussg96@gmail.com>
2022-08-21 14:00:46 +0200

    use springframework.core.env.Enviroment object to get application.properties props instead of System.getEnv()

--- WebApplication/src/main/java/com/webapplication/Configuration.java
+++ WebApplication/src/main/java/com/webapplication/Configuration.java
@@ -8,20 +8,20 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
     @Bean
-    public CachingConnectionFactory connectionFactory() {
+    public CachingConnectionFactory connectionFactory(Environment env) {
         final CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        final var env = System.getenv();
-        //connectionFactory.setHost(env.get("spring.rabbitmq.host"));
-        //final var port = Integer.valueOf(env.get("spring.rabbitmq.port"));
-        //connectionFactory.setPort(port);
-        //connectionFactory.setUsername(env.get("spring.rabbitmq.username"));
-        //connectionFactory.setPassword(env.get("spring.rabbitmq.password"));
+        connectionFactory.setHost(env.getProperty("spring.rabbitmq.host"));
+        final var port = Integer.valueOf(env.getProperty("spring.rabbitmq.port"));
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(env.getProperty("spring.rabbitmq.username"));
+        connectionFactory.setPassword(env.getProperty("spring.rabbitmq.password"));
         return connectionFactory;
     }
 
