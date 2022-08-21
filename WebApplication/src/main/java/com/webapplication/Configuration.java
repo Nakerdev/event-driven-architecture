e54dd37dd9546d9a181711983cb491bdbc18971e
Antonio Sánchez <antoniojesussg96@gmail.com>
2022-08-21 13:03:48 +0200

    created required beans and use SpingBootApplication annotation without arguments to import all packages and sub-packages

--- WebApplication/src/main/java/com/webapplication/Configuration.java
+++ WebApplication/src/main/java/com/webapplication/Configuration.java
@@ -2,9 +2,13 @@ package com.webapplication;
 
 import domain.modules.share.Infrastructure.bus.event.RabbitMqEventBus;
 import domain.modules.share.domain.bus.event.EventBus;
+import domain.modules.users.application.updateuser.UpdateUserUseCase;
+import domain.modules.users.application.updateuser.UserPostgrestRepository;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -13,22 +17,34 @@ public class Configuration {
     public CachingConnectionFactory connectionFactory() {
         final CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
         final var env = System.getenv();
-        connectionFactory.setHost(env.get("spring.rabbitmq.host"));
-        final var port = Integer.valueOf(env.get("spring.rabbitmq.port"));
-        connectionFactory.setPort(port);
-        connectionFactory.setUsername(env.get("spring.rabbitmq.username"));
-        connectionFactory.setPassword(env.get("spring.rabbitmq.password"));
+        //connectionFactory.setHost(env.get("spring.rabbitmq.host"));
+        //final var port = Integer.valueOf(env.get("spring.rabbitmq.port"));
+        //connectionFactory.setPort(port);
+        //connectionFactory.setUsername(env.get("spring.rabbitmq.username"));
+        //connectionFactory.setPassword(env.get("spring.rabbitmq.password"));
         return connectionFactory;
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate(this.connectionFactory());
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
     }
 
     @Bean
     public EventBus eventBus(final RabbitTemplate template) {
         return new RabbitMqEventBus(template);
+    }
+
+    @Bean
+    public UpdateUserUseCase updateUserUseCase(
+            EventBus eventBus,
+            UserPostgrestRepository userRepository) {
+        return new UpdateUserUseCase(eventBus, userRepository);
+    }
+
+    @Bean
+    public UserPostgrestRepository userPostgrestRepository(JdbcTemplate jdbcTemplate) {
+        return new UserPostgrestRepository(jdbcTemplate);
     }
 
 }
--- WebApplication/src/main/java/com/webapplication/EventDrivenArchitectureApplication.java
+++ WebApplication/src/main/java/com/webapplication/EventDrivenArchitectureApplication.java
@@ -1,12 +1,12 @@
 package com.webapplication;
 
+import com.webapplication.pages.home.HomeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.webApplication.pages",
-        "com.webApplication.controllers",
-        "domain.modules"})
+@SpringBootApplication()
+@ComponentScan()
 public class EventDrivenArchitectureApplication {
     public static void main(final String[] args) {
         SpringApplication.run(EventDrivenArchitectureApplication.class, args);
