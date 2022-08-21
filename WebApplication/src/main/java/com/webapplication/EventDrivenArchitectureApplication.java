8354d7fca058b1130d7b659c761eef20f768fafc
Antonio Sánchez <antoniojesussg96@gmail.com>
2022-08-21 13:11:38 +0200

    deleted unnecesary annotation

--- WebApplication/src/main/java/com/webapplication/EventDrivenArchitectureApplication.java
+++ WebApplication/src/main/java/com/webapplication/EventDrivenArchitectureApplication.java
@@ -1,12 +1,9 @@
 package com.webapplication;
 
-import com.webapplication.pages.home.HomeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan()
 public class EventDrivenArchitectureApplication {
     public static void main(final String[] args) {
         SpringApplication.run(EventDrivenArchitectureApplication.class, args);
